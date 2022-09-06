294bcf60e2e3b4d53da81b8a8a16aa6834a96d26
verners.skalbins <verners.skalbins@scandiweb.com>
2022-09-07 00:41:31 +0300

    Initial commit

---
+++ app/code/Scandiweb/Test/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @category Scandiweb
+ * @package  Scandiweb/Test
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Scandiweb_Test', __DIR__);
